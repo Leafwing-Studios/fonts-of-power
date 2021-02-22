8c6b17f8011f5fb3fe2459e6be8ee29e44e1e45d
Alice Cecile <alice.i.cecile@gmail.com>
2021-02-22 18:27:22 -0500

    More core statistics

--- src/stats.rs
+++ src/stats.rs
@@ -1,4 +1,7 @@
 use derive_more::{Deref, DerefMut};
+use std::{collections::HashSet, marker::PhantomData};
+
+use crate::skills::Skill;
 
 #[derive(Debug, PartialEq, Eq)]
 #[allow(dead_code)]
@@ -21,4 +24,64 @@ pub struct Attributes {
     expertise: AttributeVal,
     focus: AttributeVal,
     presence: AttributeVal,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+#[allow(dead_code)]
+pub enum Ideal {
+    Any,
+    Equality,
+    Harmony,
+    Liberty,
+    Progress,
+    Sanctity,
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct IdealVal(i8);
+
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub struct Ideals {
+    equality: IdealVal,
+    harmony: IdealVal,
+    liberty: IdealVal,
+    progress: IdealVal,
+    sanctity: IdealVal,
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct ProficiencyBonus(i8);
+
+#[derive(Clone, Debug, Eq, PartialEq, Deref, DerefMut)]
+pub struct Proficiencies(HashSet<Skill>);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct Life(u16);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct Essence(u16);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct Exhaustion(u16);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct Level(u8);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct Tier(u8);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct BasicAttackBonus(i8);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct SpecialAttackBonus(i8);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Deref, DerefMut)]
+pub struct BasicDefense(i8);
+
+// TODO: determine a better approach for constructing these and enforcing that T is an Attribute variant
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct SpecialDefense<T> {
+    val: i8,
+    phantom: PhantomData<T>,
 }
