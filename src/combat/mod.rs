9c4c1cd134b5d40e567fd9d5489f44c3f0849519
Alice Cecile <alice.i.cecile@gmail.com>
2021-02-23 12:13:13 -0500

    Architecture notes

--- src/combat/mod.rs
+++ src/combat/mod.rs
@@ -1,3 +1,8 @@
+/// Events are created whenever actions occur, and are hooked into
+/// by systems that power our affixes using a custom scheduler that only recomputes
+/// when something relevant has changed.
+///
+/// The turn-based nature of the game should be handled using parallel game loops.
 pub mod actions;
 pub mod attack;
 pub mod concentration;
