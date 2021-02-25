d7bc76d9535307c0be050ad0e9de364656b53436
Alice Cecile <alice.i.cecile@gmail.com>
2021-02-25 01:49:03 -0500

    Component bundles for all core actions

---
+++ src/combat/core_actions.rs
@@ -0,0 +1,239 @@
+use crate::combat::actions::{
+    Action, ActionSpeed, Actor, Range, RangeCategory, Target, TargetArity, ValidTargets,
+};
+use crate::combat::time::Duration;
+use bevy::ecs::Bundle;
+
+// TODO: derive Default for each core action
+/// Core action entity marker: these entities should never be changed at run time
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct CoreAction;
+
+#[derive(Bundle, Clone)]
+pub struct MoveAction {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct ClimbAction {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct ProneAction {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct UnproneAction {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct DefendAction {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct GrappleAction {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
+
+#[derive(Bundle, Clone)]
+pub struct ReverseGrappleAction {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Strike {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Activate {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+#[derive(Bundle, Clone)]
+pub struct BreakGrapple {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Dash {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct EssenceTap {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Guard {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Hide {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Interact {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Scan {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Shove {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Swap {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Treat {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
+
+#[derive(Bundle, Clone)]
+pub struct AoO {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Block {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    duration: Duration,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Dodge {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+}
+
+#[derive(Bundle, Clone)]
+pub struct Track {
+    core_action: CoreAction,
+    action: Action,
+    action_speed: ActionSpeed,
+    actor: Actor,
+    target: Target,
+    valid_targets: ValidTargets,
+    target_arity: TargetArity,
+    range: Range,
+    range_category: RangeCategory,
+}
